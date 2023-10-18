e584265d4361c74672a3526d84568888228b1382
Matthieu MIGNE <matthieu.migne@orange.fr>
2023-10-18 15:06:57 +0200

    Add of random input function

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from codecarbon import EmissionsTracker
+
 import brute_force
 import dp
 
-input = [0.1, 0.2, 0.4, 0.3]
+
+def random_input(n, inf=0, sup=1000):
+    import random
+    return [random.uniform(inf, sup) for _ in range(n)]
+
+#input = [0.1, 0.2, 0.4, 0.3]
+input = random_input(10)
 
 print("Brute force:")
 print(brute_force.solver(input))
